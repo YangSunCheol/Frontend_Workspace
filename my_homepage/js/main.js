0ee41189029b02da4f72c72dc25b3ed14ff22a0e
siripus <yscstar@naver.com>
2023-01-02 15:20:06 +0900

    Add 2023102

--- my_homepage/js/main.js
+++ my_homepage/js/main.js
@@ -1,15 +1,22 @@
-function chgTab(i1, i2) {
-  //탭메뉴
-  let tm = document.querySelectorAll('.tm>li');
+//로그인
+const id = document.getElementById('id');
+const password = document.getElementById('password');
+const login = document.getElementById('login');
+let errStack = 0;
 
-  //탭내용
-  let tc = document.querySelectorAll('.tc>li');
+login.addEventListener('click', () => {
+  if (id.value == 'yscstar') {
+    if (password.value == '0000') {
+      alert('로그인 되었습니다!');
+    } else {
+      alert('아이디와 비밀번호를 다시 한 번 확인해주세요!');
+      errStack++;
+    }
+  } else {
+    alert('존재하지 않는 계정입니다.');
+  }
 
-  //클래스 추가
-  tm[i1].classList.add('on');
-  tc[i1].classList.add('on');
-
-  //클래스 제거
-  tm[i2].classList.remove('on');
-  tc[i2].classList.add('on');
-}
+  if (errStack >= 5) {
+    alert('비밀번호를 5회 이상 틀리셨습니다. 비밀번호 찾기를 권장드립니다.');
+  }
+});
